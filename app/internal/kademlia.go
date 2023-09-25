4f012bec0301946342a29ed53639d7ba33892a4f
Lana Sirwan Abdulla <lana.sirwan.abdulla@arcticgroup.se>
2023-09-25 22:01:17 +0200

    Create LookupContact

--- app/internal/kademlia.go
+++ app/internal/kademlia.go
@@ -29,8 +29,59 @@ func (u *Kademlia) JoinNetwork(w *Contact) []Contact {
 	return contacts
 }
 
-func (kademlia *Kademlia) LookupContact(target *Contact) {
-	// TODO
+func (kademlia *Kademlia) Lookup(targetOrHash interface{}) ([]Contact, []byte, Contact) {
+	switch t := targetOrHash.(type) {
+	case *KademliaID:
+		// Handle contact lookup
+		contacts := kademlia.LookupContact(t)
+		return contacts, nil, Contact{}
+
+	case string:
+		// Handle data lookup
+		data, con := kademlia.LookupData(t)
+		return nil, data, con
+
+	default:
+		return nil, nil, Contact{}
+	}
+}
+
+// LookupContact "...to locate the k closest nodes to some given node ID"
+func (kademlia *Kademlia) LookupContact(target *KademliaID) (k_nodes []Contact) {
+	network := &Network{}
+	network.Node = kademlia
+	ch := make(chan []Contact)  // Channel for response
+	conCh := make(chan Contact) // Channel for response contact
+
+	//"The first alpha contacts selected are used to create a shortlist for the search."
+	shortlist := kademlia.NewLookupList(target)
+
+	// If there are fewer than alpha contacts in that bucket, contacts are selected from other buckets.
+	// The contact closest to the target key, closestNode, is noted.
+	// min
+	if shortlist.Len() < alpha {
+		// If shortlist length is less than alpha, perform the lookup for the first node asynchronously.
+		go PerformLookup(*target, shortlist.Nodes[0].Node, *network, ch, conCh)
+	} else {
+		// sending RPCs to the alpha nodes async
+		for i := 0; i < alpha; i++ {
+			go PerformLookup(*target, shortlist.Nodes[i].Node, *network, ch, conCh)
+		}
+	}
+	shortlist.updateLookupList(*target, ch, conCh, *network)
+
+	// creating the result list
+	for _, insItem := range shortlist.Nodes {
+		k_nodes = append(k_nodes, insItem.Node)
+	}
+
+	return
+}
+
+func PerformLookup(targetID KademliaID, receiver Contact, net Network, ch chan []Contact, conCh chan Contact) {
+	resultingNodes, _ := net.SendFindContactMessage(&receiver, &targetID)
+	ch <- resultingNodes
+	conCh <- receiver
 }
 
 func (kademlia *Kademlia) LookupData(hash string) {
